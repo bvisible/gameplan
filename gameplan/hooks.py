3526978e119a81dafe9780584433864b8cc0dfc3
bVisible <info@bvisible.ch>
2022-12-12 14:32:43 +0100

    Update hooks.py

--- gameplan/hooks.py
+++ gameplan/hooks.py
@@ -58,7 +58,7 @@ website_route_rules = [
 website_redirects = [
 	{"source": r"/teams(/.*)?", "target": r"/g\1"},
 	{"source": "/", "target": "/g"},
-	#{"source": "/login", "target": "/g/login"},
+	{"source": "/g/login", "target": "/login"},
 ]
 
 # Generators
